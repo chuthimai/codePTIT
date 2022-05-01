906da637e9641d55e8610914fb7bd7fa42d38ad3
Chu Mai <chuthimai18245@gmail.com>
2022-05-01 08:35:23 +0700

    sd sqrt để nhanh hơn nhưng khi đó ước số cuối  cùng nếu !=1 sẽ ko đc tính vào nên phải cộng thêm ước số cuối cùng đó

--- codePTIT/tong_uoc_so.c
+++ codePTIT/tong_uoc_so.c
@@ -5,22 +5,29 @@
 //  Created by Chu Mai on 04/03/2022.
 //
 
-//#include<stdio.h>
-//int main(){
-//    int n;
-//    scanf("%d",&n);
-//    long long sum=0;
-//    for(int i=0;i<n;i++){
-//        int a = 0;
-//        scanf("%d",&a);
-//        int j=2;
-//        while(a!=1){
-//            if(a%j==0){
-//                sum=sum+j;
-//                a=a/j;
-//            }
-//            if(a%j!=0) j++;
-//        }
-//    }
-//    printf("%lld",sum);
-//}
+#include<stdio.h>
+#include <math.h>
+
+int sumOfDivisor(int a){
+    int sum=0;
+    for(int i=2;i<sqrt(a)+1;i++){
+        while (a%i==0) {
+            sum+=i;
+            a=a/i;
+        }
+    }
+    if(a!=1) sum+=a;
+    return sum;
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    long long sum=0;
+    for(int i=0;i<n;i++){
+        int a ;
+        scanf("%d",&a);
+        sum+=sumOfDivisor(a);
+    }
+    printf("%lld\n",sum);
+}
